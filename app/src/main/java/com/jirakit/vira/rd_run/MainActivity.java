d62c04d9c363cdfc6116c1313ac0fa415b36177f
vira <virajirakit@hotmail.com>
2016-08-29 14:29:57 +0700

    Intent to SignUp finish

--- app/src/main/java/com/jirakit/vira/rd_run/MainActivity.java
+++ app/src/main/java/com/jirakit/vira/rd_run/MainActivity.java
@@ -1,7 +1,9 @@
 package com.jirakit.vira.rd_run;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }  // Main Method นี่คือเมธอด
+
+    // Get Event from Click Button
+    public void  clickSingUpMain(View view) {
+        startActivity(new Intent(MainActivity.this, SignUpActivity.class));
     }
-}
+
+
+}   // Main Class นี่คือ คลาสหลัก
