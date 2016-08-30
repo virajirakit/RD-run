5a689e85513900a96111bbb9be0b894840bfec6f
vira <virajirakit@hotmail.com>
2016-08-30 16:11:17 +0700

    insert db finish

--- app/src/main/java/com/jirakit/vira/rd_run/SignUpActivity.java
+++ app/src/main/java/com/jirakit/vira/rd_run/SignUpActivity.java
@@ -9,6 +9,16 @@ import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
+import com.squareup.okhttp.Call;
+import com.squareup.okhttp.Callback;
+import com.squareup.okhttp.FormEncodingBuilder;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.RequestBody;
+import com.squareup.okhttp.Response;
+
+import java.io.IOException;
+
 public class SignUpActivity extends AppCompatActivity {
 
     // Explicit การประกาศตัวแปร
@@ -16,6 +26,7 @@ public class SignUpActivity extends AppCompatActivity {
     private RadioGroup radioGroup;
     private RadioButton avata1RadioButton, avata2RadioButton, avata3RadioButton, avata4RadioButton, avata5RadioButton;
     private String nameString, surnameString, userString, passwordString, avataString;
+    private static final String urlPHP = "http://swiftcodingthai.com/rd/add_user_vira.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -116,7 +127,31 @@ public class SignUpActivity extends AppCompatActivity {
 
     private void uploadValueToServer() {
 
-    }
+        OkHttpClient okHttpClient = new OkHttpClient();
+        RequestBody requestBody = new FormEncodingBuilder()
+                .add("isAdd","true")
+                .add("Name",nameString)
+                .add("Surname",surnameString)
+                .add("User",userString)
+                .add("Password",passwordString)
+                .add("Avata",avataString)
+                .build();
+        Request.Builder builder = new Request.Builder();
+        Request request = builder.url(urlPHP).post(requestBody).build();
+        Call call = okHttpClient.newCall(request);
+        call.enqueue(new Callback() {
+            @Override
+            public void onFailure(Request request, IOException e) {
+
+            }
+
+            @Override
+            public void onResponse(Response response) throws IOException {
+              finish();
+            }
+        });
+
+    } // Upload
 
 
     private boolean checkChoose() {
