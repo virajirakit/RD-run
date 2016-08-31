5337b6d12c609f7e0967c4bd462283946716b241
vira <virajirakit@hotmail.com>
2016-08-31 15:08:56 +0700

    Read JSON Finish

--- app/src/main/java/com/jirakit/vira/rd_run/MainActivity.java
+++ app/src/main/java/com/jirakit/vira/rd_run/MainActivity.java
@@ -1,12 +1,20 @@
 package com.jirakit.vira.rd_run;
 
+import android.content.Context;
 import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.StrictMode;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.StaticLayout;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
 import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity {
@@ -14,6 +22,7 @@ public class MainActivity extends AppCompatActivity {
     // Explicit
     private ImageView imageView;
     private EditText userEditText, passwordEditText;
+    private String userString, passwordString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,9 +41,64 @@ public class MainActivity extends AppCompatActivity {
 
     }  // Main Method นี่คือเมธอด
 
+    //Create Inner Class
+    private class SynUser extends AsyncTask<Void, Void, String> {
+
+        // Explicit
+        private Context context;
+        private String myUserString, myPasswordString;
+        private static final String urlJSON = "http://swiftcodingthai.com/rd/get_user_master.php";
+
+        public SynUser(Context context, String myUserString, String myPasswordString) {
+            this.context = context;
+            this.myUserString = myUserString;
+            this.myPasswordString = myPasswordString;
+        }
+
+        @Override
+        protected String doInBackground(Void... params) {
+
+            try {
+                OkHttpClient okHttpClient = new OkHttpClient();
+                Request.Builder builder = new Request.Builder();
+                Request request = builder.url(urlJSON).build();
+                Response response = okHttpClient.newCall(request).execute();
+                return response.body().string();
+
+            }catch (Exception e){
+                Log.d("31AugV2","e doInBack ==> "+ e.toString());
+                return null;
+            }
+
+
+        } //doInBack
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            Log.d("31AugV2","JSoN ==> "+s);
+
+        } // onPost
+
+    } // SynUser Class
 
     public void clickSignInMain(View view){
 
+        userString = userEditText.getText().toString().trim();
+        passwordString = passwordEditText.getText().toString().trim();
+
+        //Check Space
+        if (userString.equals("")||passwordString.equals("")) {
+            //Have Space
+            MyAlert myAlert = new MyAlert();
+            myAlert.myDialog(this,R.drawable.rat48,"Have Space","Please Fill All Every Blank");
+
+        } else {
+            // No Space
+            SynUser synUser = new SynUser(this,userString,passwordString);
+            synUser.execute();
+        }
 
     } // clickSignIn
 
--- app/src/main/java/com/jirakit/vira/rd_run/SignUpActivity.java
+++ app/src/main/java/com/jirakit/vira/rd_run/SignUpActivity.java
@@ -27,7 +27,7 @@ public class SignUpActivity extends AppCompatActivity {
     private RadioGroup radioGroup;
     private RadioButton avata1RadioButton, avata2RadioButton, avata3RadioButton, avata4RadioButton, avata5RadioButton;
     private String nameString, surnameString, userString, passwordString, avataString;
-    private static final String urlPHP = "http://swiftcodingthai.com/rd/add_user_vira.php";
+    private static final String urlPHP = "http://swiftcodingthai.com/rd/add_user_master.php";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
