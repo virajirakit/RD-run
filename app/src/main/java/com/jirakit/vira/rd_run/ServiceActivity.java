9e7e257f71b2e8d9dd64ac984cf982b5f26cc0a7
vira <virajirakit@hotmail.com>
2016-09-01 16:08:17 +0700

    Get Location Finish

--- app/src/main/java/com/jirakit/vira/rd_run/ServiceActivity.java
+++ app/src/main/java/com/jirakit/vira/rd_run/ServiceActivity.java
@@ -1,7 +1,14 @@
 package com.jirakit.vira.rd_run;
 
+import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Handler;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -20,7 +27,8 @@ public class ServiceActivity extends FragmentActivity implements OnMapReadyCallb
     private TextView nameTextView,surnameTextView;
     private int[] avataInts;
     private double userLatADouble = 13.806814,userLngADouble = 100.574725; //Connection
-
+    private LocationManager locationManager;
+    private Criteria criteria;
 
 
 
@@ -41,6 +49,13 @@ public class ServiceActivity extends FragmentActivity implements OnMapReadyCallb
         nameString = getIntent().getStringExtra("Name");
         surnameString = getIntent().getStringExtra("Surname");
 
+        //Setup Location
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        criteria = new Criteria();
+        criteria.setAccuracy(Criteria.ACCURACY_FINE);
+        criteria.setAltitudeRequired(false);
+        criteria.setBearingRequired(false);
+
         //Show Text
         nameTextView.setText(nameString);
         surnameTextView.setText(surnameString);
@@ -55,8 +70,71 @@ public class ServiceActivity extends FragmentActivity implements OnMapReadyCallb
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
     } // Main Method
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        locationManager.removeUpdates(locationListener);
+        Location networkLocation  = myFindLocation(LocationManager.NETWORK_PROVIDER);
+
+        if (networkLocation != null) {
+            userLatADouble = networkLocation.getLatitude();
+            userLngADouble = networkLocation.getLongitude();
+        }
+        Location gpslocation = myFindLocation(LocationManager.GPS_PROVIDER);
+        if (gpslocation != null) {
+            userLatADouble = gpslocation.getLatitude();
+            userLngADouble = gpslocation.getLongitude();
+        }
+
+    } // OnResume
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        locationManager.removeUpdates(locationListener);
+
+    }
+
+    public Location myFindLocation(String strProvider){
+
+        Location location = null;
+        if (locationManager.isProviderEnabled(strProvider)) {
+            locationManager.requestLocationUpdates(strProvider,1000,10,locationListener);
+            location = locationManager.getLastKnownLocation(strProvider);
+
+        } else {
+            Log.d("1SepV1","Cannot find Location");
+        }
+        return location;
+    }
+
+    public LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+
+            userLatADouble = location.getLatitude();
+            userLngADouble = location.getLongitude();
+
+        } // onLocationChange
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+
+        }
+    };
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
@@ -65,5 +143,26 @@ public class ServiceActivity extends FragmentActivity implements OnMapReadyCallb
         //Setup Center of Map
         LatLng latLng = new LatLng(userLatADouble,userLngADouble);
         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,18));
+
+        // Loop
+        myLoop();
+
     } // onMap
+
+    private void myLoop() {
+
+        //To Do
+        Log.d("1SepV2","Lat ==> "+userLatADouble);
+        Log.d("1SepV2","Lng ==> "+userLngADouble);
+
+        //Post Delay
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+              myLoop();
+            }
+        },1000);
+
+    } // myLoop
 }  // Main Class
